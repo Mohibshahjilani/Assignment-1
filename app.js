1625aa1a2433bdad882bb8623d437cad962beb83
Mohibshahjilani <153758072+Mohibshahjilani@users.noreply.github.com>
2024-03-12 07:49:27 +0500

    assignment submitted

---
+++ app.js
@@ -0,0 +1,73 @@
+// chapter 1
+
+// Q 1
+var abc = " Respected VISITOR; welcome to our website!"
+alert(abc) 
+
+//   Q 2
+
+var abc ="hello"
+alert(abc) // this will display hello in the alert box. 
+
+var pass = prompt("Please Enter Your Password");
+alert("Error! Please enter a valid password.") 
+
+// Q 3
+var xyz = " Welcome to JS Land...\n Happy Coding!"
+alert(xyz) 
+
+// q4
+var shah = "welcome to JS land."
+alert(shah)
+var irtiza = "prevent this page from creating additional dialogue."
+alert(irtiza)
+                  
+            //  chapter 2
+// q1
+var userName = "syed mohib ali shah 786"
+alert(userName)
+// q2
+var myName = "syed mohib ali shah jilani"
+document.write("<h1>welcome "+myName+"</h1>")
+alert(myName)
+// q3
+
+var message ="Hello World"
+alert(message)   
+
+           //  chapter 3
+// q1
+var num1 = 2356
+var num2 = 5567  
+var totalNumber  = num1 + num2 
+var totalNumber1  = num1 - num2 
+var totalNumber2  = num1 * num2 
+var totalNumber3  = num1 / num2 
+
+alert(totalNumber) 
+alert(totalNumber1)         
+alert(totalNumber2)         
+alert(totalNumber3)         
+
+var num1 = 2 
+var num2 = 5
+var newNum = num1 + num2
+var newNum2 = num1 - num2
+var newNum3 = num1 * num2
+var newNum4 = num1 / num2
+alert(newNum)
+alert(newNum2)
+alert(newNum3)
+alert(newNum4)
+// this will display the sum of num1 and num2 which is 7 in the alert box. 
+
+// q2
+alert(234)
+alert(true)
+alert("syed mohib")
+
+ // // concatenation example:
+var a = "hello"
+var b = "world"
+var c = a + " " + b + "!"
+alert(c) // this will display hello world in the alert box
